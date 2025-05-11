afd49a31eb1c90f0ed6a0dcff4a8a278d86ede73
mrhub3117 <pathan77000@gmail.com>
2025-05-12 00:54:33 +0530

    Create Server.java
    please first run server program

---
+++ Server.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+
+public class Server
+{
+	public static void main(String[] args) {
+       try(ServerSocket serverSocket = new ServerSocket(8080)){
+       System.out.println("Server is running on port 8080");
+       
+       while(true){
+           try(Socket socket=serverSocket.accept();
+       BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+       PrintWriter out= new PrintWriter(socket.getOutputStream(),true)){
+           String received = in.readLine();
+           System.out.println("Received:"+received);
+           
+           if(received!=null){
+               String reverse = new StringBuilder(received).reverse().toString();
+               out.println(reverse);
+               System.out.println("send:"+reverse);
+               
+           }
+       }
+       }
+       
+	}
+	catch(IOException e){
+	    e.printStackTrace();
+	}
+}
+}
